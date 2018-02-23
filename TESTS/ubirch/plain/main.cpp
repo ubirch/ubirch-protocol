b7200d9a1ed38299b64ff51c606ab4f72b226f5d
Matthias L. Jugel <leo@thinkberg.com>
2018-02-23 14:26:32 +0100

    remove unused parts

--- TESTS/ubirch/plain/main.cpp
+++ TESTS/ubirch/plain/main.cpp
@@ -10,29 +10,6 @@ using namespace utest::v1;
 
 static const unsigned char UUID[16] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
 
-static const unsigned char private_key[crypto_sign_SECRETKEYBYTES] = {
-        0x69, 0x09, 0xcb, 0x3d, 0xff, 0x94, 0x43, 0x26, 0xed, 0x98, 0x72, 0x60,
-        0x1e, 0xb3, 0x3c, 0xb2, 0x2d, 0x9e, 0x20, 0xdb, 0xbb, 0xe8, 0x17, 0x34,
-        0x1c, 0x81, 0x33, 0x53, 0xda, 0xc9, 0xef, 0xbb, 0x7c, 0x76, 0xc4, 0x7c,
-        0x51, 0x61, 0xd0, 0xa0, 0x3e, 0x7a, 0xe9, 0x87, 0x01, 0x0f, 0x32, 0x4b,
-        0x87, 0x5c, 0x23, 0xda, 0x81, 0x31, 0x32, 0xcf, 0x8f, 0xfd, 0xaa, 0x55,
-        0x93, 0xe6, 0x3e, 0x6a
-};
-static const unsigned char public_key[crypto_sign_PUBLICKEYBYTES] = {
-        0x7c, 0x76, 0xc4, 0x7c, 0x51, 0x61, 0xd0, 0xa0, 0x3e, 0x7a, 0xe9, 0x87,
-        0x01, 0x0f, 0x32, 0x4b, 0x87, 0x5c, 0x23, 0xda, 0x81, 0x31, 0x32, 0xcf,
-        0x8f, 0xfd, 0xaa, 0x55, 0x93, 0xe6, 0x3e, 0x6a
-};
-
-int ed25519_sign(const char *buf, size_t len, unsigned char signature[crypto_sign_BYTES]) {
-    crypto_uint16 signedLength;
-    unsigned char *signedMessage = new unsigned char[crypto_sign_BYTES + 32];
-    crypto_sign(signedMessage, &signedLength, (const unsigned char *) buf, (crypto_uint16) len, private_key);
-    memcpy(signature, signedMessage, crypto_sign_BYTES);
-    delete[] signedMessage;
-    return 0;
-}
-
 void TestProtocolInit() {
     char dummybuffer[10];
     ubirch_protocol proto = {};
@@ -156,12 +133,7 @@ void TestProtocolMessageFinish() {
 void TestSimpleMessage() {
     char _key[20], _value[300];
     size_t encoded_size;
-
-    memset(_value, 0, sizeof(_value));
-    mbedtls_base64_encode((unsigned char *) _value, sizeof(_value), &encoded_size, public_key,
-                          crypto_sign_PUBLICKEYBYTES);
-    greentea_send_kv("publicKey", _value);
-
+    
     msgpack_sbuffer *sbuf = msgpack_sbuffer_new();
     ubirch_protocol *proto = ubirch_protocol_new(proto_plain, UBIRCH_PROTOCOL_TYPE_BIN,
                                                  sbuf, msgpack_sbuffer_write, NULL, UUID);
