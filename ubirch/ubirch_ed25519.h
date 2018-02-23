ddf853a8725cab032f81c54c51251c65f29a2682
Matthias L. Jugel <leo@thinkberg.com>
2018-02-23 14:44:46 +0100

    add crypto helper functions with key parameters

--- ubirch/ubirch_ed25519.h
+++ ubirch/ubirch_ed25519.h
@@ -36,13 +36,25 @@ extern unsigned char ed25519_public_key[crypto_sign_PUBLICKEYBYTES];    //!< ref
 
 /**
  * Function to sign a payload and return the signature.
- * @param buf the buffer with the data to sign
+ * @param data the buffer with the data to sign
  * @param len the length of the data buffer
  * @param signature the buffer to hold the returned signature
  * @return 0 on success
  * @return -1 if the signing failed
  */
-static int ed25519_sign(const unsigned char *buf, size_t len, unsigned char signature[crypto_sign_BYTES]);
+static int ed25519_sign(const unsigned char *data, size_t len, unsigned char signature[crypto_sign_BYTES]);
+
+/**
+ * Function to sign a payload with a given secret key and return the signature.
+ * @param data the buffer with the data to sign
+ * @param len the length of the data buffer
+ * @param signature the buffer to hold the returned signature
+ * @param secret_key the secret key to use
+ * @return 0 on success
+ * @return -1 if the signing failed
+ */
+static int ed25519_sign_key(const unsigned char *data, size_t len, unsigned char signature[crypto_sign_BYTES],
+                            const unsigned char secret_key[crypto_sign_SECRETKEYBYTES]);
 
 /**
  * Function to verify a data buffer with the provided signature.
@@ -54,46 +66,68 @@ static int ed25519_sign(const unsigned char *buf, size_t len, unsigned char sign
  */
 static int ed25519_verify(const unsigned char *data, size_t len, const unsigned char signature[crypto_sign_BYTES]);
 
-inline int ed25519_sign(const unsigned char *buf, const size_t len,
-                        unsigned char signature[crypto_sign_BYTES]) {
+/**
+ * Function to verify a data buffer with the given public key and the provided signature.
+ * @param data the buffer with the data to verify
+ * @param len the length of the data buffer
+ * @param signature a buffer with the corresponding signature
+ * @param public_key the public key to use for verification
+ * @return 0 on success
+ * @return -1 if the verification failed
+ */
+static int ed25519_verify(const unsigned char *data, size_t len, const unsigned char signature[crypto_sign_BYTES]);
+
+inline int ed25519_sign_key(const unsigned char *data, size_t len, unsigned char signature[crypto_sign_BYTES],
+                            const unsigned char secret_key[crypto_sign_SECRETKEYBYTES]) {
     crypto_uint16 mlen;
     unsigned char *sm = (unsigned char *) malloc(crypto_sign_BYTES + len);
     if (!sm) return -1;
 
     // sign the message
-    crypto_sign(sm, &mlen, buf, (crypto_uint16) len, ed25519_secret_key);
+    crypto_sign(sm, &mlen, data, (crypto_uint16) len, secret_key);
     memcpy(signature, sm, crypto_sign_BYTES);
 
     free(sm);
-    
+
     return 0;
 }
 
-inline int ed25519_verify(const unsigned char *buf, const size_t len,
-                          const unsigned char signature[crypto_sign_BYTES]) {
+inline int ed25519_sign(const unsigned char *data, const size_t len,
+                        unsigned char signature[crypto_sign_BYTES]) {
+    return ed25519_sign_key(data, len, signature, ed25519_secret_key);
+}
+
+inline int ed25519_verify_key(const unsigned char *data, const size_t len,
+                          const unsigned char signature[crypto_sign_BYTES],
+                              const unsigned char public_key[crypto_sign_PUBLICKEYBYTES]) {
     crypto_uint16 smlen = (crypto_uint16) (crypto_sign_BYTES + len);
     crypto_uint16 mlen;
 
     unsigned char *sm = (unsigned char *) malloc(smlen);
-    if(!sm) return -1;
+    if (!sm) return -1;
 
     unsigned char *m = (unsigned char *) malloc(smlen);
-    if(!m) {
+    if (!m) {
         free(sm);
         return -1;
     }
 
     // initialize signed message structure
     memcpy(sm, signature, crypto_sign_BYTES);
-    memcpy(sm + crypto_sign_BYTES, buf, len);
+    memcpy(sm + crypto_sign_BYTES, data, len);
 
     // verify signature
-    int ret = crypto_sign_open(m, &mlen, sm, smlen, ed25519_public_key);
+    int ret = crypto_sign_open(m, &mlen, sm, smlen, public_key);
 
     free(m);
     free(sm);
 
     return ret;
+}
+
+inline int ed25519_verify(const unsigned char *data, const size_t len,
+                              const unsigned char signature[crypto_sign_BYTES]) {
+    return ed25519_verify_key(data, len, signature, ed25519_secret_key);
 }
 
 #ifdef __cplusplus
