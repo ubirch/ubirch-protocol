3ab2dcd366c62d2237a1bf1b6f5a1b28016347f4
Roxana Meixner <roxana.meixner@gmail.com>
2019-09-12 13:07:51 +0200

    clean up

--- TESTS/host_tests/ProtocolTests.py
+++ TESTS/host_tests/ProtocolTests.py
@@ -9,23 +9,24 @@ from time import sleep
 class CryptoProtocolTests(BaseHostTest):
 
     def __unpackMessage(self, message):
+        _type = b''
         _payload = b''
         _signature = b''
-        _last_signature = b''
+
         unpacked = msgpack.unpackb(message)
         _variant = unpacked[0] & 0x000F
         _uuid = unpacked[1]
-        if _variant == 1 or _variant == 2:
-            _type = unpacked[2]
-            _payload = unpacked[3]
-        if _variant == 2:
-            _signature = unpacked[4]
         if _variant == 3:
-            _last_signature = unpacked[2]
             _type = unpacked[3]
             _payload = unpacked[4]
             _signature = unpacked[5]
-        return _variant, _uuid, _last_signature, _type, _payload, _signature
+        else:
+            _type = unpacked[2]
+            _payload = unpacked[3]
+            if _variant == 2:
+                _signature = unpacked[4]
+
+        return _variant, _uuid, _type, _payload, _signature
 
     def __verifySignature(self, message, signature):
         tohash = message[0:-66]
@@ -44,7 +45,7 @@ class CryptoProtocolTests(BaseHostTest):
         message = base64.b64decode(value.split(";", 1)[0])
         self.log("msg: "+ message.encode('hex'))
         try:
-            variant, uuid, last_signature, type, payload, signature = self.__unpackMessage(message)
+            variant, uuid, type, payload, signature = self.__unpackMessage(message)
             if type == 1:
                 self.vk = ed25519.VerifyingKey(payload['pubKey'])
             if variant == 2 or variant == 3:
@@ -55,7 +56,6 @@ class CryptoProtocolTests(BaseHostTest):
             self.send_kv("variant", variant)
             self.send_kv("uuid", uuid)
             if type == 0: self.send_kv("payload", payload)
-            # if variant == 3: self.send_kv("last signature", last_signature.encode('hex'))
 
         except Exception as e:
             self.send_kv("error", e.message)
