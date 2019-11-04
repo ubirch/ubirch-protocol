cc7dc3e4b13c1f9a333792514152f5721f33726e
Roxana Meixner <roxana.meixner@gmail.com>
2019-11-04 11:35:43 +0100

    get service url function + test

--- TESTS/ubirch/api/main.cpp
+++ TESTS/ubirch/api/main.cpp
@@ -1,4 +1,44 @@
-//
-// Created by larox on 02.11.19.
-//
+#include <unity/unity.h>
+#include "ubirch/ubirch_api.h"
+#include <ubirch/ubirch_ed25519.h>
+#include <mbedtls/base64.h>
+#include <msgpack.h>
 
+#include "utest/utest.h"
+#include "greentea-client/test_env.h"
+
+#define UBIRCH_API_KEY_SERVICE_DEMO          "https://key.demo.ubirch.com/api/keyService/v1/pubkey"
+#define UBIRCH_API_KEY_SERVICE_DEV           "https://key.dev.ubirch.com/api/keyService/v1/pubkey"
+#define UBIRCH_API_NIOMON_SERVICE_DEMO       "https://niomon.demo.ubirch.com/"
+#define UBIRCH_API_NIOMON_SERVICE_DEV        "https://niomon.dev.ubirch.com/"
+#define UBIRCH_API_VERIFICATION_SERVICE_DEMO "https://verify.demo.ubirch.com/api/upp"
+#define UBIRCH_API_VERIFICATION_SERVICE_DEV  "https://verify.dev.ubirch.com/api/upp"
+#define UBIRCH_API_DATA_SERVICE_DEMO         "https://data.demo.ubirch.com/v1"
+#define UBIRCH_API_DATA_SERVICE_DEV          "https://data.dev.ubirch.com/v1"
+
+using namespace utest::v1;
+
+void TestAPIGetServiceURL() {
+    const char *key_url_demo = ubirch_api_get_service_url(ubirch_key_service, "demo");
+    TEST_ASSERT_EQUAL_INT(strlen(UBIRCH_API_KEY_SERVICE_DEMO), strlen(key_url_demo));
+    TEST_ASSERT_EQUAL_STRING(UBIRCH_API_KEY_SERVICE_DEMO, key_url_demo);
+
+    const char *niomon_url_dev = ubirch_api_get_service_url(ubirch_niomon_service, "dev");
+    TEST_ASSERT_EQUAL_INT(strlen(UBIRCH_API_NIOMON_SERVICE_DEV), strlen(niomon_url_dev));
+    TEST_ASSERT_EQUAL_STRING(UBIRCH_API_NIOMON_SERVICE_DEV, niomon_url_dev);
+}
+
+utest::v1::status_t greentea_test_setup(const size_t number_of_cases) {
+    GREENTEA_SETUP(600, "ProtocolTests");
+    return greentea_test_setup_handler(number_of_cases);
+}
+
+int main() {
+    Case cases[] = {
+            Case("ubirch API get service URL",
+                 TestAPIGetServiceURL, greentea_case_failure_abort_handler),
+    };
+
+    Specification specification(greentea_test_setup, cases, greentea_test_teardown_handler);
+    Harness::run(specification);
+}
--- ubirch/ubirch_api.h
+++ ubirch/ubirch_api.h
@@ -9,33 +9,57 @@
 extern "C" {
 #endif
 
-#define UBIRCH_API_KEY_SERVICE          0x01
-#define UBIRCH_API_NIOMON_SERVICE       0x02
-#define UBIRCH_API_VERIFICATION_SERVICE 0x03
-#define UBIRCH_API_DATA_SERVICE         0x04
+#include "ubirch_protocol.h"
+
+#define UBIRCH_API_KEY_SERVICE          "https://key.%s.ubirch.com/api/keyService/v1/pubkey"
+#define UBIRCH_API_NIOMON_SERVICE       "https://niomon.%s.ubirch.com/"
+#define UBIRCH_API_VERIFICATION_SERVICE "https://verify.%s.ubirch.com/api/upp"
+#define UBIRCH_API_DATA_SERVICE         "https://data.%s.ubirch.com/v1"
 
 typedef enum ubirch_api_service {
-    ubirch_key_service = UBIRCH_API_KEY_SERVICE
-};
+    ubirch_key_service,
+    ubirch_niomon_service,
+    ubirch_verification_service,
+    ubirch_data_service
+} ubirch_api_service;
 
 typedef struct ubirch_api {
-    unsigned char uuid[UBIRCH_PROTOCOL_UUID_SIZE];      //!< the UUID of the sender
-    const char *auth;
-};
+    unsigned char uuid[UBIRCH_PROTOCOL_UUID_SIZE];  //!< the UUID of the sender
+    const char *auth;                               //!< the user authorization token
+} ubirch_api;
+
+inline const char *ubirch_api_get_service_url(ubirch_api_service service, const char *env) {
+    char temp_buffer[100];
+    switch (service) {
+        case ubirch_key_service:
+            sprintf(temp_buffer, UBIRCH_API_KEY_SERVICE, env);
+            break;
+        case ubirch_niomon_service:
+            sprintf(temp_buffer, UBIRCH_API_NIOMON_SERVICE, env);
+            break;
+        case ubirch_verification_service:
+            sprintf(temp_buffer, UBIRCH_API_VERIFICATION_SERVICE, env);
+            break;
+        case ubirch_data_service:
+            sprintf(temp_buffer, UBIRCH_API_DATA_SERVICE, env);
+            break;
+        default:
+            return NULL;
+    }
+    char *url = (char *) malloc(strlen(temp_buffer));
+    strcpy(url, temp_buffer);
+    return (const char *) url;
+}
 
 inline const char *ubirch_api_get_headers(ubirch_api api) {
+    const char *http_headers;
     /**
-     * TODO generate http hearder with
+     * TODO generate http header
      * "X-Ubirch-Hardware-Id": "<<api->uuid>>,
      * "X-Ubirch-Credential": b64encode(<<api->auth>>),
      * "X-Ubirch-Auth-Type": "ubirch"
      * */
     return http_headers;
-}
-
-inline const char *ubirch_api_get_service_url(ubirch_api_service service) {
-    //
-    return url
 }
 
 #ifdef __cplusplus
